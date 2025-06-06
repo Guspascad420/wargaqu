da06394c1571d8e0f1e7776843331af4a58a11bc
Guspascad420 <Paacad@gmail.com>
2025-06-06 22:50:33 +0700

    feat: integrate Firebase Core, Auth, and Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wargaqu/pages/login_choice.dart';
 import 'package:wargaqu/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: const MyApp()));
 }
 
