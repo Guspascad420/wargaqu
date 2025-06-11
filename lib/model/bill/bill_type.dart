4cb218da77b46ee20fc97a5d054219a12a0bf0e7
Guspascad420 <Paacad@gmail.com>
2025-06-11 14:46:31 +0700

    enhance BillType enum
    - Add displayName property
    - Add fromString factory constructor

--- lib/model/bill/bill_type.dart
+++ lib/model/bill/bill_type.dart
@@ -1,4 +1,15 @@
 enum BillType {
-  regular,
-  incidental
+  regular('Regular'),
+  incidental('Incidental');
+
+  const BillType(this.displayName);
+  final String displayName;
+
+  // Helper to convert from a string (e.g., from JSON) to BillType
+  static BillType fromString(String typeString) {
+    return BillType.values.firstWhere(
+          (e) => e.name.toLowerCase() == typeString.toLowerCase() || e.displayName.toLowerCase() == typeString.toLowerCase(),
+      orElse: () => BillType.regular,
+    );
+  }
 }
