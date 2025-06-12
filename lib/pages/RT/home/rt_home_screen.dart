893624a88721f75183ee84c74a3c6d4029c98502
Guspascad420 <Paacad@gmail.com>
2025-06-12 21:41:38 +0700

    Navigate to Bill Management Screen and delete unused feature

--- lib/pages/RT/home/rt_home_screen.dart
+++ lib/pages/RT/home/rt_home_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wargaqu/components/reusable_home_screen.dart';
+import 'package:wargaqu/pages/RT/bill_management/bill_management_screen.dart';
 import 'package:wargaqu/pages/RT/citizen_activity/citizen_activity_screen.dart';
-import 'package:wargaqu/theme/app_colors.dart';
 
 class RtHomeScreen extends StatefulWidget {
   const RtHomeScreen({super.key});
@@ -48,7 +48,9 @@ class _RtHomeScreenState extends State<RtHomeScreen> {
             ),
             GestureDetector(
                 onTap: () {
-
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const BillManagementScreen())
+                  );
                 },
                 child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 15),
@@ -62,7 +64,7 @@ class _RtHomeScreenState extends State<RtHomeScreen> {
                       children: [
                         Image.asset('images/group_chat.png', width: 140.w,),
                         SizedBox(height: 10.h),
-                        Text('Pengelolaan Pembayaran Iuran Bulanan', style: GoogleFonts.roboto(
+                        Text('Pengelolaan Pembayaran Iuran', style: GoogleFonts.roboto(
                           fontSize: 17,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
@@ -70,33 +72,7 @@ class _RtHomeScreenState extends State<RtHomeScreen> {
                       ],
                     )
                 )
-            ),
-            GestureDetector(
-                onTap: () {
-
-                },
-                child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    margin: const EdgeInsets.symmetric(vertical: 10),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: AppColors.secondary100,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Column(
-                      children: [
-                        Image.asset('images/world_humanitarian_day.png', width: 140.w,),
-                        SizedBox(height: 10.h),
-                        Text('Pengelolaan Pembayaran Iuran Khusus', style: GoogleFonts.roboto(
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        )),
-                      ],
-                    )
-                )
-            ),
-            const SizedBox(height: 10),
+            )
           ]
       )
     );
