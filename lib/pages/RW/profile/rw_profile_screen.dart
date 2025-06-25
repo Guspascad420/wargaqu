64ed34bd606ca5e77283eef12ae30a59527525ef
Guspascad420 <Paacad@gmail.com>
2025-06-25 09:53:53 +0700

    feat: add RwProfileScreen and RtManagementScreen

---
+++ lib/pages/RW/profile/rw_profile_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wargaqu/pages/RW/unique_code/unique_code_management_screen.dart';
+import 'package:wargaqu/pages/login_choice.dart';
+import 'package:wargaqu/providers.dart';
+import 'package:wargaqu/providers/rt_providers.dart';
+
+class RwProfileScreen extends ConsumerWidget {
+  const RwProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authService = ref.watch(authServiceProvider);
+
+    return Container(
+        margin: EdgeInsets.all(15.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Pengaturan Saya', style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(height: 10.h),
+            InkWell(
+              onTap: () {
+
+              },
+              borderRadius: BorderRadius.circular(12.0), // Biar efek ripple-nya juga rounded
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(12.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.08),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.person_outline_rounded,
+                      color: Theme.of(context).colorScheme.onSurface, // Warna ikon dari parameter
+                      size: 22.w,
+                    ),
+                    SizedBox(width: 16.w),
+                    // Teks Menu
+                    Expanded(
+                      child: Text(
+                        'Akun Saya',
+                        style: GoogleFonts.roboto(
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8.w),
+                    Icon(
+                      Icons.chevron_right_rounded,
+                      color: Colors.grey.shade500,
+                      size: 28.r,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 15.h),
+            Text('Manajemen RW', style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(height: 10.h),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => UniqueCodeManagementScreen())
+                );
+              },
+              borderRadius: BorderRadius.circular(12.0), // Biar efek ripple-nya juga rounded
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(12.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.08),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.key_rounded,
+                      color: Theme.of(context).colorScheme.onSurface, // Warna ikon dari parameter
+                      size: 22.w,
+                    ),
+                    SizedBox(width: 16.w),
+                    // Teks Menu
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Generate kode unik RT',
+                            style: GoogleFonts.roboto(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                          Text(
+                            'Buat & kelola kode registrasi untuk pengurus RT',
+                            style: GoogleFonts.roboto(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 8.w),
+                    Icon(
+                      Icons.chevron_right_rounded,
+                      color: Colors.grey.shade500,
+                      size: 28.r,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                label: Text(
+                  'Keluar',
+                  style: GoogleFonts.roboto(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red.shade600,
+                  padding: EdgeInsets.symmetric(vertical: 14.h),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.w),
+                  ),
+                  elevation: 2,
+                ),
+                onPressed: () {
+                  authService.signOut();
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const LoginChoiceScreen())
+                  );
+                },
+              ),
+            ),
+          ],
+        )
+    );
+  }
+
+}
---
+++ lib/pages/RW/rt_management/rt_management_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:wargaqu/components/rt_item_card.dart';
+import 'package:wargaqu/model/RT/rt_data.dart';
+import 'package:wargaqu/pages/RW/rt_management/new_rt_form_screen.dart';
+import 'package:wargaqu/providers/rt_providers.dart';
+import 'package:wargaqu/providers/user_providers.dart';
+
+class RtManagementScreen extends ConsumerStatefulWidget{
+  const RtManagementScreen({super.key});
+
+  @override
+  ConsumerState<RtManagementScreen> createState() => _RwCitizenScreenState();
+}
+
+class _RwCitizenScreenState extends ConsumerState<RtManagementScreen> {
+  final TextEditingController rtController = TextEditingController();
+  RtData? selectedRt;
+
+  @override
+  void dispose() {
+    rtController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final rwId = ref.watch(currentRwIdProvider);
+    final asyncRtList = ref.watch(rtListStreamProvider(rwId!));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Manajemen Warga RW',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const NewRtFormScreen())
+          );
+        },
+        label: Text('Tambah Data RT Baru'),
+        icon: Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.w),
+        child: Column(
+          children: [
+            SizedBox(height: 10.h),
+            SizedBox(
+              height: 50.h,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Cari RT...',
+                  prefixIcon: Icon(Icons.search),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12.w),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.r),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.surface,
+                ),
+              ),
+            ),
+            SizedBox(height: 10.h),
+            asyncRtList.when(
+                error: (err, stack) => Center(child: Text('Error: $err')),
+                loading: () {
+                  return CircularProgressIndicator();
+                },
+                data: (rtList) {
+                  return Expanded(
+                    child: ListView.separated(
+                        itemBuilder: (BuildContext context, int index) {
+                          final rt = rtList[index];
+                          return RtItemCard(
+                            rtId: rt.id,
+                            rtName: rt.rtName,
+                            population: rt.citizenCount ?? 0,
+                            isActive: rt.isActive,
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) => SizedBox(height: 10.h),
+                        itemCount: rtList.length
+                    ),
+                  );
+                }
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
