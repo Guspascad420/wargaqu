2daaeb96d22f2e265eda614e83a27b5c71960318
Guspascad420 <Paacad@gmail.com>
2025-06-27 17:36:53 +0700

    feat: add transaction notifier and data model

---
+++ lib/model/transaction/transaction_data.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'transaction_data.freezed.dart';
+part 'transaction_data.g.dart';
+
+DateTime? _dateTimeFromJson(Timestamp? timestamp) => timestamp?.toDate();
+Timestamp? _dateTimeToJson(DateTime? date) => date == null ? null : Timestamp.fromDate(date);
+
+@freezed
+abstract class TransactionData with _$TransactionData {
+  const TransactionData._();
+
+  const factory TransactionData.income({
+    required String description,
+    required int amount,
+    String? recipientName,
+    required String inputtedByUserId,
+    @JsonKey(fromJson: _dateTimeFromJson, toJson: _dateTimeToJson)
+    DateTime? incomeDate,
+    @JsonKey(fromJson: _dateTimeFromJson, toJson: _dateTimeToJson)
+    DateTime? createdAt,
+  }) = IncomeTransaction;
+
+  const factory TransactionData.expense({
+    required String description,
+    required int amount,
+    String? recipientName,
+    required String inputtedByUserId,
+    @JsonKey(fromJson: _dateTimeFromJson, toJson: _dateTimeToJson)
+    DateTime? expenseDate,
+    @JsonKey(fromJson: _dateTimeFromJson, toJson: _dateTimeToJson)
+    DateTime? createdAt,
+  }) = ExpenseTransaction;
+
+  factory TransactionData.fromJson(Map<String, dynamic> json) => _$TransactionDataFromJson(json);
+
+  DateTime get transactionDate {
+    DateTime date = DateTime.now();
+    switch (this) {
+      case final IncomeTransaction incomeData:
+        date = incomeData.incomeDate!;
+        break; // break is needed in switch statements
+      case final ExpenseTransaction expenseData:
+        date = expenseData.expenseDate!;
+        break;
+    }
+    return date;
+  }
+}
---
+++ lib/notifiers/transaction_notifier.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wargaqu/model/income/income.dart';
+import 'package:wargaqu/providers/rt_providers.dart';
+
+import '../model/expense/expense.dart';
+
+class TransactionNotifier extends AsyncNotifier<void> {
+  @override
+  Future<void> build() async {
+    return;
+  }
+
+  Future<void> executeNewExpense({
+    required String rtId,
+    required String description,
+    required int amount,
+    required String inputtedByUserId,
+    DateTime? expenseDate,
+  }) async {
+    state = const AsyncLoading();
+    final newExpense = Expense(
+      description: description,
+      amount: amount,
+      inputtedByUserId: inputtedByUserId,
+      expenseDate: expenseDate,
+      createdAt: DateTime.now()
+    );
+    try {
+      final rtService = ref.watch(rtServiceProvider);
+      await rtService.addExpense(rtId: rtId, newExpense: newExpense);
+      state = const AsyncData(null);
+    } catch (e) {
+      state = AsyncError(e, StackTrace.current);
+    }
+  }
+
+  Future<void> executeNewIncome({
+    required String rtId,
+    required String description,
+    required int amount,
+    required String inputtedByUserId,
+    DateTime? incomeDate,
+  }) async {
+    state = const AsyncLoading();
+    final newIncome = Income(amount: amount, inputtedByUserId: inputtedByUserId,
+        description: description);
+    try {
+      final rtService = ref.watch(rtServiceProvider);
+      await rtService.addIncome(rtId: rtId, newIncome: newIncome);
+      state = const AsyncData(null);
+    } catch (e) {
+      state = AsyncError(e, StackTrace.current);
+    }
+  }
+}
